3b64ef24b14593f49bb0d7d117c23ecfc6610722
zhonglin-h <zhonglin.h@hotmail.com>
2024-08-06 12:32:28 -0400

    runs correctly now! TODO: Authentication verification not working

--- Program.cs
+++ Program.cs
@@ -71,14 +71,8 @@ builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
